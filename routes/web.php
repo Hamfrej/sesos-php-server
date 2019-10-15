dac3c6adba239088a339b68cc5611575a6c18d65
DanielAshan <pieczynski.daniel@gmail.com>
2019-10-15 21:24:54 +0200

    Revert "Users crud (partiall)"

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::resource('users', 'UserController');
+
 Route::resource('classrooms', 'ClassroomController');
 Route::resource('lessons', 'LessonController');
 Route::resource('attendance-lists', 'AttendanceListController');
