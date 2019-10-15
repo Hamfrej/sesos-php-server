31993cb88dde83673c3f1230bca09276c8e82eeb
DanielAshan <pieczynski.daniel@gmail.com>
2019-10-15 21:34:15 +0200

    Revert "Revert "Users crud (partiall)""

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'university_id', 'nfc'
+        'name', 'email', 'password', 'university_id', 'nfc_id'
     ];
 
     /**
